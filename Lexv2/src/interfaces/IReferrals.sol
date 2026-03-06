d249bf6c325c0f75e7f8d363db934826f84b3936
Haley <115961813+ExileHaley@users.noreply.github.com>
2026-03-07 05:07:16 +0800

    Delete Lexv2 directory

--- Lexv2/src/interfaces/IReferrals.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
-
-interface IReferrals {
-    function referral(address parent, address user) external;
-    function rootAddr() external view returns(address);
-    function processStakeInfo(address user, uint256 amount) external;
-    function processUnstakeInfo(address user, uint256 amount) external;
-}
